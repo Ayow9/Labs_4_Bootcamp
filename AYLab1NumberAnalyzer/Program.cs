47b0c1ffe740d2076868eab5e14b80b6e50b36fb
Ayow9 <167594981+Ayow9@users.noreply.github.com>
2024-05-02 21:35:31 -0400

    Add files via upload
    first lab for monday due date

---
+++ AYLab1NumberAnalyzer/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Type a number between 0 and 100");
+string INPUT = Console.ReadLine();
+int inputNUM = int.Parse(INPUT);
+if (inputNUM < 60 && inputNUM % 2 != 0)
+{
+    Console.WriteLine("Odd and less than 60");
+}
+if (inputNUM < 25 && inputNUM % 2 == 0)
+{
+    Console.WriteLine("Even and less than 25");
+}
+if (inputNUM <= 60 && inputNUM % 2 == 0)
+{
+    Console.WriteLine("Even and between 26-60, inclusive");
+}
+if (inputNUM > 60 && inputNUM % 2 == 0)
+{
+    Console.WriteLine("Even and greater than 60");
+}
+if (inputNUM > 60 && inputNUM % 2 != 0)
+{
+    Console.WriteLine("Odd and greater than 60");
+}

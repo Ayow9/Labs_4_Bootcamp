510a66f31826cdf81f847e8defb92ff19ed13647
Ayow9 <alexander.yowell@gmail.com>
2024-07-24 21:32:31 -0400

    did Coffee Shop lab in class, lots of fun!

---
+++ CoffeeShopRegister/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShopRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegister.Controllers;
+
+public class UserController : Controller
+//STEP 2: Make a controller for our Model, in this case, the User. Create a controller, then name our result
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    //We're gonna make an HTTP post attribute here for STEP 5
+    //Post means from the browser or client to the controller
+    [HttpPost]
+    public IActionResult RegisterUser(User user)
+    {
+        return View();
+    }
+}
---
+++ CoffeeShopRegister/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopRegister.Models;
+
+public class User // STEP 1: MAKE OUR CLASS FOR OUR CONTROLLER 
+{
+    [Required(ErrorMessage = "Valid First Name plz")] //VV--these things in the [] brackets are attributes that are validators for the property below it--VV
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Valid Last Name plz")] //VV--validate last name--VV
+    public string LastName { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(8, ErrorMessage = "Must be 8 characters long, can be ANY characters on keyboard :)")]
+    public string Password { get; set; }
+}
